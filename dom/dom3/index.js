02a71f19eb84285f5e2b524b3b93e0a4f713a82b
yogeshtamta999-spec <yogeshtamta999.com>
2026-07-15 19:13:37 +0530

    to do list

--- dom/dom3/index.js
+++ dom/dom3/index.js
@@ -1,6 +1,36 @@
 let task=document.getElementById("task");
 console.log(task);
+let list=document.getElementById("list")
 
 document.getElementById("btn").addEventListener("click", () => {
-    console.log(task.value);
-});
+   
+    if(task.value== ""){
+        alert("Please enter a task")
+        return;
+    }else{
+        let li=document.createElement("li")
+        li.innerHTML=`${task.value}`
+
+
+            let complete=document.createElement("button");
+            complete.innerHTML="Complete"; 
+            complete.addEventListener("click", () => {
+                li.style.textDecoration="line-through"
+            })
+
+            let dlt=document.createElement("button");
+            dlt.innerHTML="remove";
+
+            dlt.addEventListener("click", () => {
+                list.removeChild(li)
+            })
+            task.value="";
+            li.appendChild(complete) 
+            li.appendChild(dlt)
+
+
+        list.appendChild(li)
+        
+    }
+
+})
